36d8d3fc990a552a81371b2597d9e3f7d34a32d9
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-07-07 17:18:36 +0900

    [feat] 공통 예외처리 구조 설정

---
+++ src/main/java/com/backend/makemyimage/global/exception/CustomException.java
@@ -0,0 +1,21 @@
+package com.backend.makemyimage.global.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class CustomException extends RuntimeException {
+    private final HttpStatus status;
+    private final String message;
+
+    public CustomException(HttpStatus status, String message) {
+        super(message);
+        this.status = status;
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ src/main/java/com/backend/makemyimage/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.backend.makemyimage.global.exception;
+
+import com.backend.makemyimage.global.api.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ApiResponse<Void>> handleCustomException(CustomException ex) {
+        ApiResponse<Void> response = new ApiResponse<>(ex.getStatus(), ex.getMessage(), null);
+        return new ResponseEntity<>(response, ex.getStatus());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Void>> handleException(Exception ex) {
+        ApiResponse<Void> response = new ApiResponse<>(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), null);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
