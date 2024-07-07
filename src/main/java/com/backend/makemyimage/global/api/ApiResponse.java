86877e72ba226d5e54c7611daf2f0e260b3e03a8
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-07-07 17:15:57 +0900

    [feat] 공통 API Response 설정

---
+++ src/main/java/com/backend/makemyimage/global/api/ApiResponse.java
@@ -0,0 +1,13 @@
+package com.backend.makemyimage.global.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private final HttpStatus status;
+    private final String message;
+    private final T data;
+}
