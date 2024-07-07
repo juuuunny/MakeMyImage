cd7fe301006ea9b7bbd0a80f0b2bf92e1020f3d2
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-07-07 16:58:24 +0900

    [feat] User 엔티티 생성

---
+++ src/main/java/com/backend/makemyimage/user/entity/User.java
@@ -0,0 +1,31 @@
+package com.backend.makemyimage.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Builder
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+}
