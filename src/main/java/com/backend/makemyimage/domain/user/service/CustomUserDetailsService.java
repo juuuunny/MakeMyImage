de6fbdf366150f58ec7224b8006e3a52836c0ded
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-07-07 18:18:24 +0900

    [feat] CustomUserDetails 구현

---
+++ src/main/java/com/backend/makemyimage/domain/user/service/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.backend.makemyimage.domain.user.service;
+
+import com.backend.makemyimage.domain.user.repository.UserRepository;
+import com.backend.makemyimage.domain.user.security.CustomUserDetails;
+import com.backend.makemyimage.global.exception.CustomException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public CustomUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        return userRepository.findByUsername(username).map(CustomUserDetails::new).orElseThrow(() -> new CustomException(HttpStatus.NOT_FOUND, "User not found"));
+
+    }
+}
