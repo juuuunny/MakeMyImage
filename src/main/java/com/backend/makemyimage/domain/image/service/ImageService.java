02eb6c2eb502b60b81a2ee9063ddbf214e600dac
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-07-09 17:11:28 +0900

    [feat] 카카오 karlo 통해서 생성한 이미지 데이터 베이스 성공

--- src/main/java/com/backend/makemyimage/domain/image/service/ImageService.java
+++ src/main/java/com/backend/makemyimage/domain/image/service/ImageService.java
@@ -2,12 +2,19 @@ package com.backend.makemyimage.domain.image.service;
 
 import com.backend.makemyimage.domain.image.dto.request.ImageCreateRequest;
 import com.backend.makemyimage.domain.image.dto.response.ImageCreateResponse;
+import com.backend.makemyimage.domain.image.entity.Image;
 import com.backend.makemyimage.domain.image.repository.ImageRepository;
+import com.backend.makemyimage.domain.user.dto.response.UserInfoResponse;
+import com.backend.makemyimage.domain.user.entity.User;
+import com.backend.makemyimage.domain.user.repository.UserRepository;
+import com.backend.makemyimage.domain.user.security.CustomUserDetails;
 import com.backend.makemyimage.global.exception.CustomException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,12 +24,26 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class ImageService {
 
+    private final UserRepository userRepository;
     private final ImageRepository imageRepository;
     private final KakaoKarloService kakaoKarloService;
 
     public String createImage(ImageCreateRequest imageCreateRequest) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        String email = userDetails.getEmail(); // 사용자 이메일
 
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new CustomException(HttpStatus.NOT_FOUND, "User not found"));
 
-        return KakaoKarloService.getImage(imageCreateRequest);
+        String image= kakaoKarloService.getImage(imageCreateRequest);
+
+        imageRepository.save(
+                Image.builder()
+                        .userId(user.getId())
+                        .imageUrl(image)
+                        .keyword(imageCreateRequest.getKeyword())
+                        .build());
+        return image;
     }
 }
