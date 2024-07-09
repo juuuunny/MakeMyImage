7fa51b6ff431a0379d279a4d4da44aacb0eceff5
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-07-09 22:13:09 +0900

    [feat] 쓸모 없는 import 삭제

--- src/main/java/com/backend/makemyimage/domain/image/service/ImageService.java
+++ src/main/java/com/backend/makemyimage/domain/image/service/ImageService.java
@@ -7,13 +7,10 @@ import com.backend.makemyimage.domain.image.entity.Image;
 import com.backend.makemyimage.domain.image.repository.ImageRepository;
 import com.backend.makemyimage.domain.user.entity.User;
 import com.backend.makemyimage.domain.user.repository.UserRepository;
-import com.backend.makemyimage.domain.user.security.CustomUserDetails;
 import com.backend.makemyimage.global.config.FindUserByToken;
 import com.backend.makemyimage.global.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
